9f1817bba6678e9c9512800957eadbcc11602ae1
Jitendra-singh-cell <jitendrasingh9x221@gmail.com>
2024-11-04 22:05:51 +0500

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-function getFormvalue() {
-    //Write your code here
+function getFormvalue(event) {
+    event.preventDefault(); // Prevent the default form submission
 
+    // Get the form element
+    const form = document.getElementById('form1');
+
+    // Get the values of the first name and last name fields
+    const firstName = form.elements['fname'].value;
+    const lastName = form.elements['lname'].value;
+
+    // Display the full name in an alert box
+    alert(firstName + ' ' + lastName);
 }
